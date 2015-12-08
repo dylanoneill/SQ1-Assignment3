b3a14eaec1b52c719c9d2ad40cd0a5d46612d072
Dylan O'Neill <dylan.o.neill@hotmail.com>
2015-12-08 16:24:02 -0500

    Created code to prevent area calculation is triangle is not right

--- SQ1-A3/TriangleUnitTesting/TriangleTest.cs
+++ SQ1-A3/TriangleUnitTesting/TriangleTest.cs
@@ -84,7 +84,7 @@ namespace TriangleUnitTesting{
         public void CalculateAreaNonRightAngleTest()
         {
             Triangle t = new Triangle();
-            Assert.AreEqual(0, (int)t.CalculateHypotenuse(70, 70, 70));
+            Assert.AreEqual(0, (int)t.CalculateHypotenuse(70, 70, 100));
         }
     }
 }
